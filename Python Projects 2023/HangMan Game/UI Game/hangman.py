a7221c05f1bb429650b30d34539f9da22f7f9927
Sami RAJICHI <59516997+sami-rajichi@users.noreply.github.com>
2023-06-29 17:32:39 +0100

    Add files via upload

--- Python Projects 2023/HangMan Game/UI Game/hangman.py
+++ Python Projects 2023/HangMan Game/UI Game/hangman.py
@@ -6,7 +6,6 @@ Created on Tue Jun 27 23:35:00 2023
 @project: HangMan
 """
 
-from pygame_setup import PygameSetup
 from random import choice
 
 
@@ -14,135 +13,63 @@ class HangMan:
 
     def __init__(self):
         """
-        This method meant to initialize the game with a custom title, fetch the words 
+        This method meant to initialize fetch the words 
         from a text file and initialize the class attributes.
         """
-        welcome: str = 'Welcome To HangMan Game'
-        print(f'{welcome:-^40}')
-
         # Initialize attributes
-        self.__reset()
+        self.reset()
 
-    def __reset(self):
+    def reset(self):
         """
         This method meant to initialize all class attributes.
         """
         # Retrieve all the words from wordlist file
         with open('./wordlist.txt', 'r') as f:
-            self.__words: list[str] = f.read().split()
-            self.__word: str = choice(self.__words).upper()
+            self.words: list[str] = f.read().split()
+            self.word: str = choice(self.words).upper()
         self.tries: int = 6
-        self.__guessed_letters: list[str] = []
-        self.__guessed_words: list[str] = []
-        self.__guessed: bool = False
-        self.__word_completion: str = '-' * len(self.__word)
+        self.guessed_letters: list[str] = []
+        self.guessed: bool = False
+        self.word_completion: str = '-' * len(self.word)
 
-    def __check_letter_guess(self, guess: str, word: str):
+    def check_letter_guess(self, guess: str, word: str):
         """
         This method is executed when the user guess a letter.
-        It consists of 3 tests (conditions):
-            - When the letter had been already guessed.
+        It consists of 2 tests (conditions):
             - When the letter doesn't exist in the secret word.
             - When the letter correctly guessed.
         """
-        # When the letter had been already guessed.
-        if guess in self.__guessed_letters:
-            print('You have already guessed that letter. Choose again.')
-        # When the letter doesn't exist in the secret word.
-        elif guess not in word:
-            print(f'{guess} not in the word.')
+        if guess not in word:
             # Decrement the number of tries and add the letter to the guessed letters list
             # So that the user get noticed to not type it again.
             self.tries -= 1
-            self.__guessed_letters.append(guess)
+            self.guessed_letters.append(guess)
         # When the letter correctly guessed.
         else:
             # Add the letter to the list
-            self.__guessed_letters.append(guess)
+            self.guessed_letters.append(guess)
             # Get the indices of the appropriate letters in the word
             indices: list[int] = [
                 i for i, letter in enumerate(word) if letter == guess]
             # Replace the '-' that corresponds of the positions of the letter(s)
-            word_list: list[str] = list(self.__word_completion)
+            word_list: list[str] = list(self.word_completion)
             for index in indices:
                 word_list[index] = guess
-            self.__word_completion = ''.join(word_list)
+            self.word_completion = ''.join(word_list)
             # If no '-' found, the guessed flag is being switched to True.
             # As a result, the game ends
-            if '-' not in self.__word_completion:
-                self.__guessed = True
+            if '-' not in self.word_completion:
+                self.guessed = True
 
-    def __check_word_guess(self, word: str, right_word: str):
-        """
-        This method is executed when the user guesses a word.
-        It consists of 3 tests (conditions):
-            - When the word had been already guessed and not the right one.
-            - When the word is not equal to the right secret word.
-            - When the word correctly guessed.
-        """
-        # When the word had been already guessed and not the right one.
-        if word in self.__guessed_words:
-            print('You have already guessed that word. Think again.')
-        # When the word is not equal to the right secret word.
-        elif word not in right_word:
-            print('This is not the correct word.')
-            # Decrement the number of tries and add the letter to the guessed words list
-            # So that the user get noticed to not type it again.
-            self.tries -= 1
-            self.__guessed_words.append(word)
-        # When the word correctly guessed.
-        else:
-            # If no '-' found, the guessed flag is being switched to True.
-            # As a result, the game ends
-            self.__guessed = True
-            self.__word_completion = right_word
-
-    def __user_guides(self):
-        """
-        This method shows some guidelines to the user like the stages and word completion state.
-        """
-        print(self.__word_completion)
-        if len(self.__guessed_letters) > 0:
-            print('* Used Letters: ', end='')
-            print(*self.__guessed_letters, sep='-')
-        if len(self.__guessed_words) > 0:
-            print('* Used Words: ', end='')
-            print(*self.__guessed_words, sep='-')
-        print('\n')
-
-    def start_game(self):
+    def start_game(self, user_guess: str):
         """
         This method represents the starting point of the game.
         """
-        # Call the guidelines
-        self.__user_guides()
-        # Loop till the guessing evaluated to True or the number of attempts increased to 0.
-        while not self.__guessed and self.tries > 0:
-            # Take the user input and make it in uppercase
-            user_guess = input('Guess a letter or word: ').upper()
-            # This evaluates if the length of the input is equal to 1
-            # and make sure it's alphabetic
-            if len(user_guess) == 1 and user_guess.isalpha():
-                # Call the check_letter_guess method
-                self.__check_letter_guess(user_guess, self.__word)
-            # This evaluates if the length of the guessed word is equal to the right word
-            # and make sure it's alphabetic as well
-            elif len(user_guess) == len(self.__word) and user_guess.isalpha():
-                # Call the check_word_guess method
-                self.__check_word_guess(user_guess, self.__word)
-            else:
-                print('Not a valid guess.')
-            self.__user_guides()
-        if self.__guessed:
-            print(f'\nYes! The secret word is {self.__word}. You won !!\n')
+        # This evaluates if the length of the input is equal to 1
+        # and make sure it's alphabetic
+        if len(user_guess) == 1 and user_guess.isalpha():
+            # Call the check_letter_guess method
+            self.check_letter_guess(user_guess, self.word)
         else:
-            print(
-                f'Sorry, you ran out of tries. The word was {self.__word}. Maybe next time!\n')
-        # Call the reset method to initialize attributes
-        self.__reset()
-
-
-if __name__ == '__main__':
-    hangman: HangMan = HangMan()
-    pygame_setup: PygameSetup = PygameSetup()
-    pygame_setup.run(hangman.tries)
+            print('Not a valid guess.')
+        # self.reset()
--- Python Projects 2023/HangMan Game/UI Game/pygame_setup.py
+++ Python Projects 2023/HangMan Game/UI Game/pygame_setup.py
@@ -7,6 +7,8 @@ Created on Wed Jun 28 07:22:04 2023
 """
 
 import pygame
+from math import sqrt
+from hangman import HangMan
 
 
 class PygameSetup:
@@ -32,15 +34,23 @@ class PygameSetup:
         self.startX: float = round(
             (self.WIDTH - (self.RADIUS*2 + self.GAP) * 13) / 2)
         self.startY: int = 375
+        self.rectX: float = (self.WIDTH / 2) - 235
+        self.rectY: float = (self.HEIGHT / 2) + 100
+        self.rectWidth: float = 150
+        self.rectHeight: float = 60
+        self.play_again = True
         for i in range(26):
             x: float = self.startX + self.GAP*2 + \
                 ((self.RADIUS*2 + self.GAP) * (i % 13))
             y: float = self.startY + ((self.RADIUS*2 + self.GAP) * (i // 13))
-            self.letters.append((x, y, chr(65 + i)))
+            self.letters.append([x, y, chr(65 + i), True])
         # Set text fonts
         self.LETTER_FONT: pygame.Font = pygame.font.SysFont('comicsans', 28)
+        self.WORD_FONT = pygame.font.SysFont('comicsans', 46)
+        self.TITLE_FONT = pygame.font.SysFont('comicsans', 60)
 
     def __init__(self):
+        self.hangman = HangMan()
         self.__setup_display()
         self.__game_variables()
         self.__load_images()
@@ -50,34 +60,116 @@ class PygameSetup:
             if event.type == pygame.QUIT:
                 self.run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                print(pygame.mouse.get_pos())
+                mouseX, mouseY = pygame.mouse.get_pos()
+                for letter in self.letters:
+                    x, y, ltr, visible = letter
+                    if visible:
+                        distance: float = sqrt(
+                            (x - mouseX)**2 + (y - mouseY)**2)
+                        if distance <= self.RADIUS:
+                            self.hangman.start_game(ltr)
+                            letter[3] = False
 
     def __load_images(self):
         for i in range((7)):
             self.images.append(pygame.image.load(
                 f'./images/hangman{str(i)}.png'))
 
+    def __render_text(self, text: str, font: object, x: float, y: float):
+        text: pygame.Surface = font.render(
+            text, 1, self.BLACK)
+        self.win.blit(text, (x - text.get_width()/2,
+                      y - text.get_height()/2))
+
+    def game_status(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX, mouseY = pygame.mouse.get_pos()
+                if (mouseX > self.rectX and mouseX < self.rectX + self.rectWidth) and (mouseY > self.rectY and mouseY < self.rectY + self.rectHeight):
+                    self.__game_variables()
+                    self.__load_images()
+                    self.hangman = HangMan()
+                    self.play_again = True
+                if (mouseX > self.rectX*2 + self.rectWidth and mouseX < self.rectX*2 + self.rectWidth*2) and (mouseY > self.rectY and mouseY < self.rectY + self.rectHeight):
+                    self.run = False
+
+    def __draw_play_again_button(self):
+        pygame.draw.rect(self.win, self.BLACK,
+                         (self.rectX, self.rectY, self.rectWidth, self.rectHeight), width=2)
+        pygame.draw.rect(self.win, self.BLACK,
+                         (self.rectX*2 + self.rectWidth, self.rectY, self.rectWidth, self.rectHeight), width=2)
+        text: pygame.Surface = self.LETTER_FONT.render(
+            'Play Again', 1, self.BLACK)
+        self.win.blit(text, (self.rectX + (self.rectWidth - text.get_width()) / 2,
+                      self.rectY + (self.rectHeight - text.get_height()) / 2))
+        text: pygame.Surface = self.LETTER_FONT.render(
+            'Quit', 1, self.BLACK)
+        self.win.blit(text, (self.rectX*2 + self.rectWidth + (self.rectWidth - text.get_width()) / 2,
+                      self.rectY + (self.rectHeight - text.get_height()) / 2))
+        self.game_status()
+
+    def __game_result(self, res: str):
+        if res == 'win':
+            self.win.fill(self.WHITE)
+            self.__render_text('You WON !', self.TITLE_FONT,
+                               (self.WIDTH/2), (self.HEIGHT/2) - 40)
+            self.__draw_play_again_button()
+        else:
+            self.win.fill(self.WHITE)
+            self.__render_text('Game Over !', self.TITLE_FONT,
+                               (self.WIDTH/2), (self.HEIGHT/2) - 80)
+            self.__render_text('The word was: ' + self.hangman.word, self.WORD_FONT,
+                               (self.WIDTH/2), (self.HEIGHT/2))
+            self.__draw_play_again_button()
+        pygame.display.update()
+
     def __draw_button(self):
         for letter in self.letters:
-            x, y, ltr = letter
-            pygame.draw.circle(self.win, self.BLACK, (x, y), self.RADIUS, 2)
-            text: pygame.Surface = self.LETTER_FONT.render(ltr, 1, self.BLACK)
-            self.win.blit(text, (x - (text.get_width()/2),
-                          y - (text.get_height()/2)))
+            x, y, ltr, visible = letter
+            if visible:
+                pygame.draw.circle(self.win, self.BLACK,
+                                   (x, y), self.RADIUS, 2)
+                text: pygame.Surface = self.LETTER_FONT.render(
+                    ltr, 1, self.BLACK)
+                self.win.blit(text, (x - (text.get_width()/2),
+                              y - (text.get_height()/2)))
 
     def __update_display(self, image: int):
         self.win.fill(self.WHITE)
         self.__draw_button()
-        self.win.blit(self.images[image], (150, 100))
+        self.__render_text(self.hangman.word_completion,
+                           self.WORD_FONT, (self.WIDTH / 2) + 100, (self.HEIGHT / 2) - 20)
+        self.__render_text('HangMan Game',
+                           self.TITLE_FONT, (self.WIDTH / 2), 40)
+        self.win.blit(self.images[image], (150, 110))
         pygame.display.update()
 
-    def run(self, image: int):
+    def run(self):
         """
         This method tends to set the game loop and run the game
         """
         clock: object = pygame.time.Clock()
+        image: int = self.hangman.tries
         self.__update_display(image)
         while self.run:
-            clock.tick(self.FPS)
-            self.__event_trigger()
+            if self.play_again:
+                clock.tick(self.FPS)
+                image: int = self.hangman.tries
+                self.__event_trigger()
+                self.__update_display(image)
+                if self.hangman.guessed or image == 0:
+                    self.play_again = False
+            else:
+                if self.hangman.guessed:
+                    pygame.time.delay(600)
+                    self.__game_result('win')
+                if image == 0:
+                    self.__game_result('lose')
         pygame.quit()
+
+
+if __name__ == '__main__':
+    pygame_setup: PygameSetup = PygameSetup()
+    pygame_setup.run()
