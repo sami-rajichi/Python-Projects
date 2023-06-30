6fa1b8be98c16ce40bea68f430b4110216f29a82
Sami RAJICHI <59516997+sami-rajichi@users.noreply.github.com>
2023-06-30 16:16:13 +0100

    Add files via upload

--- Python Projects 2023/Dice Simulator/dice_simulator.py
+++ Python Projects 2023/Dice Simulator/dice_simulator.py
@@ -9,20 +9,73 @@ from random import randint
 
 class DiceSimulator:
 
-    # Define a method called get_user_input to ask the user for the number of rolls
-    # To ensure that the method is private, adding two _ at the beginning is mandatory
+    def __init__(self):
+        self.dice_art: dict = {
+            1: ("┌─────────┐",
+                "│         │",
+                "│    ●    │",
+                "│         │",
+                "└─────────┘"),
+            2: ("┌─────────┐",
+                "│  ●      │",
+                "│         │",
+                "│      ●  │",
+                "└─────────┘"),
+            3: ("┌─────────┐",
+                "│  ●      │",
+                "│    ●    │",
+                "│      ●  │",
+                "└─────────┘"),
+            4: ("┌─────────┐",
+                "│  ●   ●  │",
+                "│         │",
+                "│  ●   ●  │",
+                "└─────────┘"),
+            5: ("┌─────────┐",
+                "│  ●   ●  │",
+                "│    ●    │",
+                "│  ●   ●  │",
+                "└─────────┘"),
+            6: ("┌─────────┐",
+                "│  ●   ●  │",
+                "│  ●   ●  │",
+                "│  ●   ●  │",
+                "└─────────┘")
+        }
+
     def __get_user_input(self) -> str:
+        """
+            Defines a method called get_user_input to ask the user for the number of rolls.
+            To ensure that the method is private, adding two _ at the beginning is mandatory
+        """
         return input('Haw many dice would you like to roll? ')
 
-    # Define a method called roll_dice to store each roll output
     def __roll_dice(self, number_of_rolls: int = 2) -> list[int]:
+        """
+            Defines a method called roll_dice to store each roll output
+        """
         # The number_of_rolls should be greater than 0, otherwise an error is thrown
         if number_of_rolls <= 0:
             raise ValueError
         # Return a list of rolls outputs that are generated randomly
-        return [randint(1, 6) for roll in range(number_of_rolls)]
+        self.rand_rolls: list[int] = [
+            randint(1, 6) for roll in range(number_of_rolls)]
+        return [self.dice_art[roll] for roll in self.rand_rolls]
+
+    def __dice_artworks_displayer(self, user_input: str):
+        """
+        Displaying all the generated dice artworks on the screen
+        """
+        rolls: list[str] = self.__roll_dice(int(user_input))
+        for roll in range(5):
+            for line in range(len(rolls)):
+                print(rolls[line][roll], end=' ')
+            print()  # Jumping to a new line
 
     def roll(self):
+        """
+            Presents the starter of the game
+        """
         print('---------- Welcome to Dice Simulator ----------\n')
         while True:
             # Handle exception if the user input is invalid
@@ -32,10 +85,9 @@ class DiceSimulator:
                 if user_input.lower() == 'exit':
                     break
                 # Get the rolls values
-                rolls: list[int] = self.__roll_dice(int(user_input))
-                print(*rolls, sep=', ')
+                self.__dice_artworks_displayer(user_input)
                 # Print out the sun of the outputs
-                print(f'** Sum of the rolls:{sum(rolls): >5}\n')
+                print(f'** Sum of the rolls:{sum(self.rand_rolls): >5}\n')
             except ValueError:
                 print('Please, enter a valid number.')
 
