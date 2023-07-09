2bd8c6b8c9d6b6dd400249e34ee837039834e5d2
Sami RAJICHI <59516997+sami-rajichi@users.noreply.github.com>
2023-07-09 20:47:30 +0100

    Add files via upload

--- Python Projects 2023/QRCode Generator/qrcode_generator.py
+++ Python Projects 2023/QRCode Generator/qrcode_generator.py
@@ -12,10 +12,20 @@ import qrcode
 class CustomQr:
 
     def __init__(self, qr_size: int, padding: int):
-        self.qr = qrcode.QRCode(box_size=qr_size)
+        """
+        This method represents the initializer of the class.
+        It creates an instance from the QRCode class, and requires a size and box padding 
+        as parameters
+        """
+        self.qr = qrcode.QRCode(box_size=qr_size, border=padding)
 
     def create_qr(self, code_color: str, background_color: str, filename: str):
-
+        """
+        This method serves in simulating a QRCode:
+            1- It takes first some data that will be stored as code
+            2- Makes an image that holds the data 
+            3- Then, saves it within the location of the this python script
+        """
         data: str = input('Enter data: ')
 
         try:
@@ -28,6 +38,7 @@ class CustomQr:
             print(f'{e}')
 
 
+# Main function
 if __name__ == '__main__':
     qr = CustomQr(30, 3)
     qr.create_qr('purple', 'white', 'sample.png')
